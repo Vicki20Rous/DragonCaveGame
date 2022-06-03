4528ccf208d973f036ca417cd4422b624221c3a3
Victoria Rouser <vic86rous@gmail.com>
2022-06-03 18:42:43 -0500

    made some adjustments added intro to game

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("You are in a land full of dragons. In front of you, you see two caves.\n" +
+                "In one cave, the dragon is friendly and will share his treasure with you.\n" +
+                "The other dragon is greedy and hungry and will eat you on sight.\n" +
+                "Which cave will you go into? (1 or 2)");
         Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
 
+        if (input == 1) {
+            System.out.println();
+        }
 
     }
 }
