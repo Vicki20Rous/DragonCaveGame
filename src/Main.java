a576fe5e1a4105f49e2960c2c5a369803f475f1d
Victoria Rouser <vic86rous@gmail.com>
2022-06-03 18:14:11 -0500

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner in = new Scanner(System.in);
+
+
     }
 }
